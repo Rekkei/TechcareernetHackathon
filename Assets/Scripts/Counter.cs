423d3d1e4b1704cdd769b3738607132afa9b77da
Tuna Yavuz <tunayavuz.95@hotmail.com>
2023-12-16 21:18:19 +0300

    behaviour eklendi

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public int liderlik;
+    public int yaraticilik;
+    public int analitikdusunce;
+    
+
+    void Start()
+    {
+         liderlik=0;
+        yaraticilik= 0;
+         analitikdusunce=0;
+    }
+
+}
