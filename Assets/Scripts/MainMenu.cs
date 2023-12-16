91164191adb132f12ef774d8a8b856c069dc9948
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2023-12-16 19:23:17 +0300

    Main Menu Added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Tavern");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
