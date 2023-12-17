94278b4a3273942b53e63834e680e898e39846d3
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2023-12-17 14:36:25 +0300

    Hotfix 2.0

--- Assets/Scripts/Behaviour.cs
+++ Assets/Scripts/Behaviour.cs
@@ -16,19 +16,19 @@ public class Behavour : MonoBehaviour
 
     string Finder()
     {
-        if (ct.A >= 36) return "Proje Yöneticisi";
-        if (ct.A >= 18 && ct.A <= 30 && ct.C >= 30) return "CEO veya Genel Müdür";
-        if (ct.A >= 18 && ct.A <= 30 && ct.B >= 30) return "Ýþ Geliþtirme Yöneticisi";
+        if (ct.A >= 30) return "Proje Yöneticisi";
+        if (ct.A >= 15 && ct.A <= 25 && ct.C >= 25) return "CEO veya Genel Müdür";
+        if (ct.A >= 15 && ct.A <= 25 && ct.B >= 25) return "Ýþ Geliþtirme Yöneticisi";
 
 
-        if (ct.B >= 36) return "Veri Analizi Uzmaný";
-        if (ct.B >= 18 && ct.B <= 30 && ct.C >= 30) return "Teknoloji ve Bilgi Yönetimi Uzmaný";
-        if (ct.B >= 18 && ct.B <= 30 && ct.A >= 30) return "Risk Yönetimi Uzmaný";
+        if (ct.B >= 30) return "Veri Analizi Uzmaný";
+        if (ct.B >= 15 && ct.B <= 25 && ct.C >= 25) return "Teknoloji ve Bilgi Yönetimi Uzmaný";
+        if (ct.B >= 15 && ct.B <= 25 && ct.A >= 25) return "Risk Yönetimi Uzmaný";
 
 
-        if (ct.C >= 36) return "Ýnovasyon Lideri";
-        if (ct.C >= 18 && ct.C <= 30 && ct.A >= 30) return "Ýnsan Kaynaklarý Yöneticisi";
-        if (ct.C >= 18 && ct.C <= 30 && ct.B >= 30) return "Ýþ Geliþtirme Yöneticisi";
+        if (ct.C >= 30) return "Ýnovasyon Lideri";
+        if (ct.C >= 15 && ct.C <= 25 && ct.A >= 25) return "Ýnsan Kaynaklarý Yöneticisi";
+        if (ct.C >= 15 && ct.C <= 25 && ct.B >= 25) return "Ýþ Geliþtirme Yöneticisi";
 
         return "Bu kiþi için uygun bir iþ yok";
     }
